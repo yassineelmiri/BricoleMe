c43eb055a9d0d6348d2c9e33cf2faf7ef84c8eb4
Yassine Elmiri <144792335+yassineelmiri@users.noreply.github.com>
2024-02-27 14:00:00 +0100

    la dernier version de cette project

--- resources/views/auth/customer/register.blade.php
+++ resources/views/auth/customer/register.blade.php
@@ -3,7 +3,17 @@
 @section('title','Customer Registration')
 
 @section('content')
-    <div class="w-[100vw] h-[100vh] flex mx-auto justify-center items-center " id="register_page_one">
+    <div class="w-[100vw] h-[100vh] flex flex-col gap-[1rem] mx-auto justify-center items-center " id="register_page_one">
+        @if ($errors->any())
+            <div class="w-[50%] bg-red-300 font-medium text-red-700 rounded-lg px-[2.5%] py-[0.5rem]">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <section class="sm:w-[100%] h-[100vh] lg:bg-slate-100 lg:w-[70%] lg:h-[80vh] lg:rounded-xl">
             <div class="container h-full px-6 py-24">
                 <div
@@ -16,12 +26,12 @@
                     </div>
 
                     <div class="md:w-8/12 lg:ml-6 lg:w-5/12">
-                        <form>
+                        <form action="{{route('customer.register')}}" method="POST">
+                            @csrf
                             <input
                                 class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-transparent"
                                 type="text" placeholder="Username"
-                                id= "username"
-                                name="username"
+                                name="name"
                             />
 
                             <input
@@ -36,12 +46,11 @@
                                 id= "pw"
                                 name="password"
                                 type="password" placeholder="Password"/>
+
                             <input
                                 class="w-full px-8  py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-transparent mt-5"
-                                id= "confirm_pw"
-                                name="password"
+                                name="confirm_password"
                                 type="password" placeholder="Confirm Password"/>
-
 
                             <input
                                 class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-transparent mt-5"
@@ -50,7 +59,7 @@
                                 name="phone_number"
                             />
 
-                            <select id="city" class="mt-5 mb-[1.5rem] w-full px-8 h-[40px] rounded-lg font-medium bg-gray-100 placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-transparent">
+                            <select id="city" name="city" class="mt-5 mb-[1.5rem] w-full px-8 h-[40px] rounded-lg font-medium bg-gray-100 placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-transparent">
                                 <option value="">Select Your City</option>
                                 @foreach($cities as $city)
                                     <option value="{{$city}}">{{$city}}</option>
@@ -58,7 +67,7 @@
                             </select>
 
                             <footer class="flex justify-center px-8 pb-8 pt-4 ">
-                                <button  class="rounded-lg w-[30%] py-2 bg-indigo-500 hover:bg-indigo-400 text-white font-medium focus:shadow-outline focus:outline-none next">
+                                <button type="submit" class="rounded-lg w-[30%] py-2 bg-indigo-500 hover:bg-indigo-400 text-white font-medium focus:shadow-outline focus:outline-none next">
                                     Register
                                 </button>
                             </footer>
